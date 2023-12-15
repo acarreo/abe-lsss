e25d0624e7c99a8c56d68c32541846af0c51da59
Adam Oumar Abdel-rahman <adam92oumar@gitlab.com>
2023-12-15 15:02:21 +0100

    Install RELIC

---
+++ compile/install-relic.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+RELIC_TAG="0.6.0"
+CURVE=${CURVE:-bls12-381}
+
+sudo apt-get update
+sudo apt-get install -y git cmake make g++ libgmp-dev vim flex libfl-dev pkg-config
+
+git clone --branch ${RELIC_TAG} --depth 1 https://github.com/relic-toolkit/relic /tmp/relic
+
+mkdir -p /tmp/relic/build-${CURVE}
+cd /tmp/relic/build-${CURVE}
+../preset/x64-pbc-${CURVE}.sh ..
+make -j && sudo make install
+
