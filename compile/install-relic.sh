a35605a3e45f7beafbea7c327065552ae7dc7701
Adam Oumar Abdel-rahman <adam92oumar@gitlab.com>
2024-04-24 17:44:36 +0200

    - Add openssl and libssl-dev to the list of dependencies for relic - Compile relic with shared libraries instead of static libraries

--- compile/install-relic.sh
+++ compile/install-relic.sh
@@ -4,12 +4,13 @@ RELIC_TAG="0.6.0"
 CURVE=${CURVE:-bls12-381}
 
 sudo apt-get update
-sudo apt-get install -y git cmake make g++ libgmp-dev vim flex libfl-dev pkg-config
+sudo apt-get install -y git cmake make g++ libgmp-dev vim flex libfl-dev pkg-config openssl libssl-dev
 
 git clone --branch ${RELIC_TAG} --depth 1 https://github.com/relic-toolkit/relic /tmp/relic
 
 mkdir -p /tmp/relic/build-${CURVE}
 cd /tmp/relic/build-${CURVE}
+sed -i 's/-DSHLIB=OFF -DSTBIN=ON/-DSHLIB=ON -DSTBIN=OFF/' ../preset/x64-pbc-${CURVE}.sh
 ../preset/x64-pbc-${CURVE}.sh ..
 make -j && sudo make install
 sudo cp /tmp/relic/src/md/blake2.h /usr/local/include/
