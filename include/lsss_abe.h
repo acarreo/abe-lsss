b8812e7b8b1afd2b0195e33e82f88fc27aeadab3
Adam Oumar Abdel-rahman <adam92oumar@gitlab.com>
2024-04-29 16:54:42 +0200

    Add new headers file for the project

---
+++ include/lsss_abe.h
@@ -0,0 +1,15 @@
+#ifndef __LSSS_ABE_H__
+#define __LSSS_ABE_H__
+
+#include "abe/zabe.h"
+#include "abe/zcontainer.h"
+#include "abe/zcryptoutils.h"
+#include "abe/zinteger.h"
+#include "abe/zkdf.h"
+#include "abe/zkey.h"
+#include "abe/zkeystore.h"
+#include "abe/zpairing.h"
+#include "abe/zsymcrypto.h"
+#include "abe/zsymkey.h"
+
+#endif // __LSSS_ABE_H__
