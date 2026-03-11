b99d15446a9075d363c0fdaf4c3b02b57b37472c
Adam Oumar Abdel-rahman <47461147+acarreo@users.noreply.github.com>
2026-03-11 16:10:51 +0000

    Disable relic tests and benchmarks to speed up installation.

--- compile/install-relic.sh
+++ compile/install-relic.sh
@@ -10,7 +10,7 @@ git clone --branch ${RELIC_TAG} --depth 1 https://github.com/relic-toolkit/relic
 
 mkdir -p /tmp/relic/build-${CURVE}
 cd /tmp/relic/build-${CURVE}
-sed -i 's/-DSHLIB=OFF -DSTBIN=ON/-DSHLIB=ON -DSTBIN=OFF/' ../preset/x64-pbc-${CURVE}.sh
+sed -i 's/-DSHLIB=OFF -DSTBIN=ON/-DSHLIB=ON -DSTBIN=OFF -DTESTS=OFF -DBENCH=OFF/' ../preset/x64-pbc-${CURVE}.sh
 ../preset/x64-pbc-${CURVE}.sh ..
 make -j && sudo make install
 sudo cp /tmp/relic/src/md/blake2.h /usr/local/include/
